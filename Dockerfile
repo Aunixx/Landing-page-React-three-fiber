b31dc60f04ed417d5331d0bfbb7e6eb38022be47
TK-DTP-171 <sahil.arwand2000@gmail.com>
2023-02-24 21:34:15 +0500

    sdsa

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM 168237867270.dkr.ecr.us-east-1.amazonaws.com/node-16:latest
-WORKDIR "/nitroleague-website"
-COPY package*.json ./
-RUN npm install
-COPY . .
-RUN npm run build
-EXPOSE 3000
-CMD ["npm", "start"]
--- docker-compose-pull.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-git pull
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 168237867270.dkr.ecr.us-east-1.amazonaws.com
-/usr/local/bin/docker-compose -f /home/ubuntu/nitroleague-website/docker-compose.yml pull
-/usr/local/bin/docker-compose -f /home/ubuntu/nitroleague-website/docker-compose.yml up -d
-aws cloudfront create-invalidation --distribution-id E3W38XDGWS7B9I --paths "/*"
